7fe21f17df56e78705bfa9beb88c06756113fde4
David-van-der-Linden <61505634+David-van-der-Linden@users.noreply.github.com>
2024-05-30 17:01:27 +0200

    Deleted my_secrets.py

--- src/my_secrets.py
+++
@@ -1,5 +0,0 @@
-# you path:
-my_path = 'C:/example/path/todo/change/me/qap-thesis/' # make sure to end with /
-# using full path is recommended, however
-# you can use my_path = './' when running from the qap-thesis/ directory, and
-# you can use my_path = '../' when running from the qap-thesis/src/ directory.
